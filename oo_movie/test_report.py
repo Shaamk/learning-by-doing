8661352a2342a2c758221e834ff5ad956da60d2b
Shaam Kesharie <shaam.kesharie@fox-it.com>
2023-06-26 09:59:27 +0200

    changes

--- oo_movie/test_report.py
+++ oo_movie/test_report.py
@@ -13,31 +13,31 @@ def test_movie_properties_or_construction():
     assert result.rating == 10
 
 def test_str_method():
-    result = Movie('lamaland', [], 2025, "nobody", 10).__str__()
+    result = Movie('lamaland', ['lama'], 2025, "nobody", 10).__str__()
     assert result == 'lamaland, 10'
 
 def test_str_method_second():
-    movie = Movie('lamaland', [], 2025, "nobody", 10)
+    movie = Movie('lamaland', ['lama'], 2025, "nobody", 10)
     result = movie.__str__()
     assert result == 'lamaland, 10'
 
 def test_str_method_third():
-    movie = Movie('lamaland', [], 2025, "nobody", 10)
+    movie = Movie('lamaland', ['lama'], 2025, "nobody", 10)
     result = str(movie)
     assert result == 'lamaland, 10'
 
-def test_lt_method():
-    movie = Movie('lamaland', [], 2025, "nobody", 9)
+def test_lt_method():# less-than operator
+    movie = Movie('lamaland', ['lama'], 2025, "nobody", 9)
     movie1 = Movie("The Transformers: The Movie", ["Peter Cullen", "Frank Welker", "Leonard Nimoy", "Judd Nelson", "Robert Stack", "Orson Welles"], 1986, "action", 10)
     assert movie < movie1
 
 def test_lt_method_other_way_around():
-    movie = Movie('lamaland', [], 2025, "nobody", 9)
+    movie = Movie('lamaland', ['lama'], 2025, "nobody", 9)
     movie1 = Movie("The Transformers: The Movie", ["Peter Cullen", "Frank Welker", "Leonard Nimoy", "Judd Nelson", "Robert Stack", "Orson Welles"], 1986, "action", 10)
     assert not movie1 < movie
 
 def test_lt_method_again():
-    movie = Movie('lamaland', [], 2025, "nobody", 9)
+    movie = Movie('lamaland', ['lama'], 2025, "nobody", 9)
     movie1 = Movie("The Transformers: The Movie", ["Peter Cullen", "Frank Welker", "Leonard Nimoy", "Judd Nelson", "Robert Stack", "Orson Welles"], 1986, "action", 10)
     with pytest.raises(TypeError):
         assert movie < 19
